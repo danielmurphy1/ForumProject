51d118d74266683d8a87c122cd423281dc631a7c
danielmurphy1 <daniel.murphy.1@gmail.com>
2023-10-05 15:06:09 -0400

    added button for signup

--- src/app/components/login-form/login-form.component.ts
+++ src/app/components/login-form/login-form.component.ts
@@ -8,10 +8,14 @@ import { Component, OnInit } from '@angular/core';
 export class LoginFormComponent implements OnInit{
   username: string;
   password: string;
-  
+
   constructor(){}
 
   ngOnInit(): void {
     
+  }
+
+  test(){
+    console.log("clicked here")
   }
 }
