0739be4c9ac3fbf6c322c287c5c411121c480504
danielmurphy1 <daniel.murphy.1@gmail.com>
2023-10-19 09:48:22 -0400

    added Data folder with ForumDataContext file. added dev conn string to appsettings.Development.json

---
+++ Data/ForumDataContext.cs
@@ -0,0 +1,62 @@
+﻿using ForumProject.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ForumProject.Data
+{
+    public class ForumDataContext : DbContext
+    {
+        private readonly IConfiguration _configuration;
+
+        public ForumDataContext(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        public DbSet<Board> Boards { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Post> Posts { get; set; }
+        public DbSet<Reply> Replies { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var connectionString = _configuration.GetSection("ConnectionString").Value;
+            optionsBuilder.UseNpgsql(connectionString);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Post>()
+                .HasMany(p => p.ReplyMessages)
+                .WithOne(r => r.Post)
+                .HasForeignKey(r => r.PostId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            modelBuilder.Entity<User>()
+                .HasMany(u => u.Posts)
+                .WithOne(p => p.User)
+                .HasForeignKey(p => p.UserId)
+                .OnDelete(DeleteBehavior.NoAction);
+
+            modelBuilder.Entity<User>()
+                .HasMany(u => u.Replies)
+                .WithOne(p => p.User)
+                .HasForeignKey(p => p.UserId)
+                .OnDelete(DeleteBehavior.NoAction);
+
+            modelBuilder.Entity<Board>()
+                .HasMany(b => b.Posts)
+                .WithOne(p => p.Board)
+                .HasForeignKey(p => p.BoardId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            modelBuilder.Entity<User>(entity =>
+            {
+                entity.HasIndex(e => e.Username).IsUnique();
+                entity.HasIndex(e => e.Email).IsUnique();
+            });
+
+            base.OnModelCreating(modelBuilder);
+        }
+
+        
+    }
+}
