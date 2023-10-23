6b016656b73d891db1cf58925eb6059b772d119a
danielmurphy1 <daniel.murphy.1@gmail.com>
2023-10-23 12:27:18 -0400

    using DTOs for background data so that model entities are not exposed to client

---
+++ DataTransferObjects/PostDTO.cs
@@ -0,0 +1,12 @@
+﻿using ForumProject.Models;
+
+namespace ForumProject.DTOs
+{
+    public class PostDTO
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Body { get; set; }
+        public UserDTO User { get; set; }
+    }
+}
---
+++ DataTransferObjects/UserDTO.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ForumProject.DTOs
+{
+    public class UserDTO
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
